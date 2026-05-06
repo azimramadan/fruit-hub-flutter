3087c506ffdd4eb12a13ee916881563e6a36eaed
Abdelazim Ramadan <Abdelazim Ramadan>
2026-05-06 15:37:48 +0300

    init: create Flutter project for FruitHub

---
+++ android/app/src/main/kotlin/com/fruithub/fruit_hub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fruithub.fruit_hub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitHub());
+}
+
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('FruitHub'))),
+    );
+  }
+}
